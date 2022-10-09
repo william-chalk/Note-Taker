083d68c33db6d23cb3593b1447c52212013cd52a
William Chalk <3halk1@gmail.com>
2022-10-09 19:19:56 -0400

    added routes

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+});
